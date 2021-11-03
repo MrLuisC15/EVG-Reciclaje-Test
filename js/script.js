56cd575e0dd6cc45e4a636c8437941ff47505a81
DCRDAW <91120910+DCRDAW@users.noreply.github.com>
2021-11-03 10:37:52 +0100

    v0.95

--- js/script.js
+++ js/script.js
@@ -3,16 +3,22 @@ let verde=document.getElementById("pverde");
 let azul=document.getElementById("pazul");
 let amarillo=document.getElementById("pamarilla");
 let draggable=document.getElementById("drag1")
+let draggable2=document.getElementById("drag2")
+let draggable3=document.getElementById("drag3")
 
 verde.setAttribute("ondrop", "drop(event)")
 azul.setAttribute("ondrop", "drop(event)")
 amarillo.setAttribute("ondrop", "drop(event)")
 draggable.setAttribute("draggable","true")
+draggable2.setAttribute("draggable","true")
+draggable3.setAttribute("draggable","true")
 
 verde.setAttribute("ondragover", "allowDrop(event)")
 azul.setAttribute("ondragover", "allowDrop(event)")
 amarillo.setAttribute("ondragover", "allowDrop(event)")
 draggable.setAttribute("ondragstart","drag(event)")
+draggable2.setAttribute("ondragstart","drag(event)")
+draggable3.setAttribute("ondragstart","drag(event)")
 
 function allowDrop(ev) {
     ev.preventDefault();

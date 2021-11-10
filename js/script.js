72e18e8dae66af89de3ecc21be2e1cad58c77836
LuisCMr <81901473+MrLuisC15@users.noreply.github.com>
2021-11-10 08:53:07 +0100

    Items tienen clase

--- js/script.js
+++ js/script.js
@@ -71,6 +71,8 @@ class Vista{
         img.insertBefore(img, document.getElementsByClassName('items'))
       img.setAttribute('src', nuevoItem)
       img.classList.add('items')
+      img.classList.add(preguntaTipo.bind(nuevoItem))
+
       //img.style.top = 10+'px'
       //img.style.left = Math.floor(Math.random() *85)+'%'
       img.setAttribute("id", this.contadorItems)
@@ -81,24 +83,59 @@ class Vista{
       console.log(this.contadorItems);
       
    }
+
+   preguntaTipo(nuevoItem){
+      for(let i=0; i<Modelo.itemsAmarillo.length;i++) {
+         if(nuevoItem==Modelo.itemsAmarillo[i]){
+            return 'itemAmarillo'
+         }
+      }
+
+      for(let i=0; i<Modelo.itemsAzul.length;i++) {
+         if(nuevoItem==Modelo.itemsAzul[i]){
+            return 'itemAzul'
+         }
+      }
+
+      for(let i=0; i<Modelo.itemsVerde.length;i++) {
+         if(nuevoItem==Modelo.itemsVerde[i]){
+            return 'itemVerde'
+         }
+      }
+   }
 }
 
 class Modelo{
    constructor(){
-      this.items = ['carton.png', 'lata.png', 'mansana.png', 'pelota.png']
+      this.itemsAmarillo = ['lata.png']
+      this.itemsAzul = ['carton.png','pelota.png']
+      this.itemsVerde = [ 'mansana.png']
    }
 
    crearItem(){
-      return 'img/'+this.items[Math.floor(Math.random() *this.items.length)]
+      let random = Math.floor(Math.random() *3)
+
+      switch (random) {
+         case 0:
+            return 'img/'+this.itemsAmarillo[Math.floor(Math.random() *this.itemsAmarillo.length)]
+            break;
+      
+         case 1:
+            return 'img/'+this.itemsAzul[Math.floor(Math.random() *this.itemsAzul.length)]
+            break;
+      
+         case 2:
+            return 'img/'+this.itemsVerde[Math.floor(Math.random() *this.itemsVerde.length)]
+            break;
+      
+      }
+      
    }
 }
 
 let verde=document.getElementById("pverde");
 let azul=document.getElementById("pazul");
 let amarillo=document.getElementById("pamarilla");
-let draggable=document.getElementById("drag1")
-let draggable2=document.getElementById("drag2")
-let draggable3=document.getElementById("drag3")
 
 
 verde.setAttribute("ondrop", "drop(event)")

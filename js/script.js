a4f6fcfbc81f703e45e82b2a634842156ee7666f
kiliann <wilian_kilian@hotmail.com>
2021-11-24 23:21:34 +0100

    GAME OVER

--- js/script.js
+++ js/script.js
@@ -299,7 +299,11 @@ class Juego{
       window.clearInterval(generadorItems)
       perder.play()
       let divPerder= document.createElement('div')
+      let img = document.createElement('img')
       this.divPrincipal.appendChild(divPerder)
+      divPerder.classList('gameOver')
+      divPerder.appendChild(img)
+      img.setAttribute('src', 'img/GameOver.png')
       divPerder.textContent=`Has perdido en el nivel ${nivel}`
       pausa=0
    }

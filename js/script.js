7399aa604fcd9b3919c2863c799d7c5416ad63fe
LuisCMr <81901473+MrLuisC15@users.noreply.github.com>
2021-11-24 09:22:19 +0100

    Puntos arreglados

--- js/script.js
+++ js/script.js
@@ -259,7 +259,7 @@ class Juego{
       let contadorItems = 0
       this.vista.dibujar(divPrincipal, nuevoItem, contadorItems)
       contadorItems++
-      if((nivel==2 || nivel==3 || nivel==4)&& puntos==1){
+      if((nivel==2 || nivel==3 || nivel==4)&& puntos==0){
          window.clearInterval(generadorItems)
          this.intervaloItem()
       }
